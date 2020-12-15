230a18ba9b9e3b46bf2e7b4268f026336057cdb2
Ramón Sánchez <rsanchez@afi.es>
2020-12-15 17:11:51 +0100

    add party in selectbox

--- app_twitter.py
+++ app_twitter.py
@@ -20,10 +20,12 @@ with open(r"data_politicos.json", "r", encoding = 'utf-8') as read_file:
     data = json.load(read_file)
 data = dict((key,d[key]) for d in data for key in d)
 
-
+perfiles = []
+for key in data.keys():    
+    perfiles = perfiles + [f"{key} ({data[key]['partido']})"]
+        
 perfil = st.selectbox('Elige un político', list(data.keys()))
-perfil = data[perfil]['twitter_name']
-
+perfil = data[key]['twitter_name']
 # =============================================================================
 # Carga de datos
 # =============================================================================
